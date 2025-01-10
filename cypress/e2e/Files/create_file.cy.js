e6e7a2413130001d3efe07388cfb3e77ed129300
lokesh-chandana <lokeshchandana96@gmail.com>
2025-01-10 10:15:32 +0530

    Update create_file.cy.js

--- cypress/e2e/Files/create_file.cy.js
+++ cypress/e2e/Files/create_file.cy.js
@@ -1,5 +1,16 @@
-describe('PHPEngine',() => {
-    it('Tables Creating', () => {
-        cy.visit('https://v2.backendmaker.com/apimaker/');
+describe('PHPEngine', () => {
+    it('Create new file', () => {
+        cy.visit('https://v2.backendmaker.com/apimaker/')
+        cy.get(':nth-child(1) > :nth-child(2) > .form-control').type('admin')
+        cy.get(':nth-child(2) > :nth-child(2) > .form-control').type('Admin123!@#')
+        cy.get('.btn').click()
+        cy.get('input[placeholder="Captcha Code"]').clear().type('223366')
+        cy.get('.btn').click().wait(500)
+
+        cy.get('a').contains('testing-app').click();
+        cy.get('a').contains('Files').click();
+        cy.get('button').contains('Create File').click().wait(500);
+        cy.get('input[placeholder="Name"]').clear().type('testing.html');
+        cy.get('.btn.btn-primary.btn-sm').contains('Create').click();
     })
 })
